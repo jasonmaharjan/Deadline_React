a417efbcff43cdd94a2d5f6673198847c4777dbf
Jason <jason8maharjan@gmail.com>
2021-01-09 15:32:31 +0545

    Completed dark mode feature

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectDarkMode } from '../../redux/course/course.selectors';
+
 import logo from '../../logo.svg';
 import { Link } from 'react-router-dom';
 import { toggleDarkMode } from '../../redux/course/course.actions';
 
 import "./header.styles.scss";
 
-const Header = ({toggleDarkMode}) => {
+const Header = ({toggleDarkMode, darkMode}) => {
+
+   const [checked, setChecked] = useState(darkMode);
+
+   const handleCheckbox = () => {
+      setChecked(!checked)
+      toggleDarkMode();
+   }
    return (
       <div className="App">
             <header className="App-header">
@@ -17,9 +27,14 @@ const Header = ({toggleDarkMode}) => {
                   </span>
                </span>
 
-               <button className = "App-header-toggle-button" onClick = {toggleDarkMode}>
-                     o
-               </button>
+               <div className = "App-btn">
+                  <input className = "App-btn-input" 
+                         type = "checkbox" id = "toggle-btn" 
+                         checked = {checked}
+                         onChange = {handleCheckbox}
+                  />
+                  <label className = "App-btn-label" for = "toggle-btn" />
+               </div>
             </header>
 
             <div className = "route">
@@ -37,8 +52,12 @@ const Header = ({toggleDarkMode}) => {
       );
 }
 
+const MapStateToProps = createStructuredSelector({
+   darkMode: selectDarkMode
+});
+
 const MapDispatchToProps = dispatch => ({
    toggleDarkMode: () => dispatch(toggleDarkMode())
 });
 
-export default connect(null, MapDispatchToProps)(Header);
+export default connect(MapStateToProps, MapDispatchToProps)(Header);
