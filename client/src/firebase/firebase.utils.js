78a6b85726af7da11635aecc71b8e598942834c0
unknown <jason8maharjan@gmail.com>
2023-01-10 08:55:57 +0545

    Cleanup

--- client/src/firebase/firebase.utils.js
+++ client/src/firebase/firebase.utils.js
@@ -1,19 +1,19 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const config = {
-   apiKey: "AIzaSyDo7wnA02zqVTik0X8dTPqXU_TIFf91o6Q",
-   authDomain: "deadlinetracker-9c5d8.firebaseapp.com",
-   projectId: "deadlinetracker-9c5d8",
-   storageBucket: "deadlinetracker-9c5d8.appspot.com",
-   messagingSenderId: "446199022865",
-   appId: "1:446199022865:web:442975ef45235f9f3a38b0"
- };
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID,
+};
 
- // Firestore returns 2 objects: either query or snapshot (Maybe be doc or collection)
- // Ref is an object that represents the current place in database (saves data)
- // Snapshot object gets the required data (gets data)
+// Firestore returns 2 objects: either query or snapshot (Maybe be doc or collection)
+// Ref is an object that represents the current place in database (saves data)
+// Snapshot object gets the required data (gets data)
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
@@ -21,7 +21,8 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   const userRef = firestore.doc(`users/${userAuth.uid}`); // Document Reference
   const snapShot = await userRef.get(); // Document Snapshot
 
-  if (!snapShot.exists) {   // create new userRef i.e, user data if a snapShot doesn't exist
+  if (!snapShot.exists) {
+    // create new userRef i.e, user data if a snapShot doesn't exist
     const { displayName, email } = userAuth;
     const createdAt = new Date();
 
@@ -31,72 +32,72 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         email,
         createdAt,
         deadlines: [],
-        ...additionalData
+        ...additionalData,
       });
       return userRef;
+    } catch (error) {
+      console.log("Error creating user", error.message);
     }
-    catch(error){
-      console.log('Error creating user', error.message);
-    }
-  }
-  
-  else return userRef;
-}
+  } else return userRef;
+};
 
 // create a new collection and set documents inside it
-export const addCollectionAndDocuments = async(collectionKey, objectsToAdd) => {
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
   const collectionRef = firestore.collection(collectionKey);
 
   // store documents of a collection in a batch
   const batch = firestore.batch();
-  objectsToAdd.forEach(obj => {
+  objectsToAdd.forEach((obj) => {
     const newDocRef = collectionRef.doc(); // document reference with random unique ID
     batch.set(newDocRef, obj);
   });
 
   return await batch.commit();
-}
+};
 
 // basically get data from firestore and store it in reducer!!
 // for converting the collection snapshot array to an object
-export const convertCollectionSnapshotToMap = (collections) => {  
-  const transformedCollection = collections.docs.map(doc => {
-    const {title, items} = doc.data();
+export const convertCollectionSnapshotToMap = (collections) => {
+  const transformedCollection = collections.docs.map((doc) => {
+    const { title, items } = doc.data();
     // return an object with extra props
     return {
       routeName: encodeURI(title.toLowerCase()),
       id: doc.id,
       title,
-      items
-    }
+      items,
+    };
   });
 
   return transformedCollection.reduce((accumulator, collection) => {
     accumulator[collection.title.toLowerCase()] = collection;
     return accumulator;
   }, {});
-}
+};
 
 // for user persistence
 export const getCurrentUser = () => {
   return new Promise((resolve, reject) => {
-    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
       unsubscribe();
       resolve(userAuth);
     }, reject);
   });
-}
+};
 
- firebase.initializeApp(config);
+firebase.initializeApp(config);
 
- export const auth = firebase.auth();
- export const firestore = firebase.firestore();
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
 
- export const googleProvider = new firebase.auth.GoogleAuthProvider();
- googleProvider.setCustomParameters({ prompt: 'select_account'});
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: "select_account" });
 
- export const signInWithGoogle = () => {
-   auth.signInWithPopup(googleProvider);
- }
+export const signInWithGoogle = () => {
+  auth.signInWithPopup(googleProvider);
+};
 
- export default firebase;
+export default firebase;
