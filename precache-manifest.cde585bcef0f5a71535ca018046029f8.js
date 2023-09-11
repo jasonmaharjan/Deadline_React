577dc0e383fc325acab0b3e147540e50eafe9e06
jasonmaharjan <jason8maharjan@gmail.com>
2023-09-11 15:23:30 +0545

    Updates

---
+++ precache-manifest.cde585bcef0f5a71535ca018046029f8.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2e2e9155124e7ec61aec4ad49099ef5",
+    "url": "/Deadline_Tracker/index.html"
+  },
+  {
+    "revision": "be5d2911c3894f6e15da",
+    "url": "/Deadline_Tracker/static/css/2.324ccd6c.chunk.css"
+  },
+  {
+    "revision": "82856e080f31b60005c0",
+    "url": "/Deadline_Tracker/static/css/main.747398ce.chunk.css"
+  },
+  {
+    "revision": "be5d2911c3894f6e15da",
+    "url": "/Deadline_Tracker/static/js/2.4494d60d.chunk.js"
+  },
+  {
+    "revision": "a63c2c1dc9a8093487e6afcd3dbd8493",
+    "url": "/Deadline_Tracker/static/js/2.4494d60d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82856e080f31b60005c0",
+    "url": "/Deadline_Tracker/static/js/main.44eba62a.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/Deadline_Tracker/static/js/main.44eba62a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebbc84976195a1dce5ef",
+    "url": "/Deadline_Tracker/static/js/runtime-main.82d22293.js"
+  },
+  {
+    "revision": "3d87305db50e91ced638adae2e6f3460",
+    "url": "/Deadline_Tracker/static/media/cross.3d87305d.svg"
+  },
+  {
+    "revision": "dddf0c8b8c5216332645392d30620d57",
+    "url": "/Deadline_Tracker/static/media/edit.dddf0c8b.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Deadline_Tracker/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "81f46e26a4c9fdff22b47640ab1a96ba",
+    "url": "/Deadline_Tracker/static/media/red_warning.81f46e26.svg"
+  },
+  {
+    "revision": "619ca765ea87a44857950a8feec6ad9e",
+    "url": "/Deadline_Tracker/static/media/searchButton.619ca765.svg"
+  },
+  {
+    "revision": "90f60d0a2ad3a6ab974b6929a0f2c006",
+    "url": "/Deadline_Tracker/static/media/settings.90f60d0a.svg"
+  },
+  {
+    "revision": "28ac7e4d8c3d58f734557a3a16fb3fb4",
+    "url": "/Deadline_Tracker/static/media/sort.28ac7e4d.svg"
+  },
+  {
+    "revision": "7cca510b3d93cb2cb3007a017f6b5137",
+    "url": "/Deadline_Tracker/static/media/yellow_warning.7cca510b.svg"
+  }
+]);
